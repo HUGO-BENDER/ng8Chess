dd1b84cfbce23f7d8078ba6fb486f330a37fb07d
Hugo Bender <benderhugo@hotmail.com>
2019-08-19 17:12:06 +0200

    install @ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8Chess';
+  selectedLang: string;
+  listLanguages = [
+    { id: 'en', name: 'English' },
+    { id: 'fr', name: 'French' },
+    { id: 'ca', name: 'Catalan' },
+    { id: 'es', name: 'Spanish' }
+  ];
+
+  constructor(// router: Router,
+    // private sidenavService: SidenavService
+    private translate: TranslateService) {
+
+    this.translate.addLangs(this.listLanguages.map(l => l.id));
+    this.translate.setDefaultLang('es');
+
+    const browserLang = this.translate.getBrowserLang();
+    this.selectedLang = browserLang.match(/en|fr|ca|es/) ? browserLang : 'en';
+    this.translate.use(this.selectedLang);
+
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// -- App components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// --Translate
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +24,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
